9956e40c5a71a942f7604a5615f6ccbb48b97c8d
Sonu-Kumhar <kumharsonu899@gmail.com>
2025-08-20 23:37:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,26 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Register from './components/register';
-import Login from './components/login';
+
+import Register from './components/Register';
+import Login from './components/Login';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
-import MySessions from './components/Mysessions';
+import MySessions from './components/MySessions';
 import PublishedSessions from './components/PublishedSessions';
 import EditSession from './components/EditSession';
 import CreateSession from './components/CreateSession';
 import Navbar from './components/Navbar';
-import { useEffect } from 'react'; 
 
 function AppContent() {
   const location = useLocation();
 
-  // Hide navbar on these pages
-  const hideNavbarRoutes = ['/', '/login', '/register'];
-  const showNavbar = !hideNavbarRoutes.includes(location.pathname);
+  // Always show Navbar on all routes
+  const showNavbar = true;
 
   return (
     <>
-      <Navbar/>
+      {showNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
@@ -33,7 +32,6 @@ function AppContent() {
         <Route path="/dashboard/published-sessions" element={<PublishedSessions />} />
         <Route path="/dashboard/edit-session/:id" element={<EditSession />} />
       </Routes>
-      <ToastContainer position="top-right" autoClose={2000} />
     </>
   );
 }
@@ -42,6 +40,7 @@ function App() {
   return (
     <BrowserRouter>
       <AppContent />
+      {/* Keep only one ToastContainer */}
       <ToastContainer position="top-right" autoClose={2000} />
     </BrowserRouter>
   );
